da9649bd2764f7b3a7f996b54b18bf7fead4c847
Alex Foley <Foley@Tabris.local>
2015-10-21 20:06:03 +0200

    toggle now actually works
    I also added some headers, so my linter doesn’t give warnings, hope
    nobody minds

--- js/switch.js
+++ js/switch.js
@@ -1,7 +1,44 @@
+/*global $ */
+/*global document */
+
 $(function() {
 
   $('.switch').change(function(){
-    $(this).toggleClass('checked');
+      
+      if ($(".switch").hasClass("checked")) {
+            $(this).toggleClass('checked');
+            $("script[src='js/particles.js']").remove();
+            $( "#particles-js" ).remove();
+            
+          document.cookie="visualizer-off";
+          
+
+      } 
+      else {
+      
+      $(this).toggleClass('checked');
+          
+      $( "<div id='particles-js'></div>" ).insertAfter( "#footer" );
+      
+      var headID = document.getElementsByTagName("head")[0];         
+      var newScript = document.createElement('script');
+      newScript.type = 'text/javascript';
+      newScript.src = 'js/particles.js';
+      headID.appendChild(newScript);
+          
+          document.cookie="visualizer-on";
+      }
+      
   });
 
+});
+
+$(document).ready(function() {
+    
+    var cookie = document.cookie;
+    if (cookie == "visualizer-on") {
+        
+        $('.switch').click(); 
+    }
+    
 });
