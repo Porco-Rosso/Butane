8f91e581ce16d24187dfc61bb44e0eba0ed375bc
Porco-Rosso <Foley@Tabris.local>
2016-04-22 18:37:41 +0200

    beautify js

--- js/pageloadanimation.js
+++ js/pageloadanimation.js
@@ -2,18 +2,19 @@
 /*global document */
 
 $(document).ready(function ($) {
-	
-	 $(function () {
-     $('.hidden-on-load').removeClass('hidden-on-load');
- 					});
-	$('.navbar').addClass('animated bounceInDown');	
+
+	$(function () {
+		$('.hidden-on-load').removeClass('hidden-on-load');
+	});
+
+	$('.navbar').addClass('animated bounceInDown');
 	$('#footer').addClass('animated bounceInUp');
-	
+
 	$('.wrapper').hide();
-	setTimeout(function(){
-	$('.wrapper').show();
-    $('.wrapper').addClass('animated fadeIn');
-}, 600);
-	
-	
+	setTimeout(function () {
+		$('.wrapper').show();
+		$('.wrapper').addClass('animated fadeIn');
+	}, 600);
+
+
 });
