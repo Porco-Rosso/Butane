60a22a2bce09ed3c149b0dd0a154c07cd29e462b
Porco-Rosso <Porco-Rosso@users.noreply.github.com>
2016-05-30 19:10:55 +0200

    inserted table

--- js/app.js
+++ js/app.js
@@ -198,11 +198,13 @@ $(document).ready(function ($) {
 					
 					var popovercontent = "<div class='ad'><!-- BEGIN ADREACTOR CODE --><div id='avp_zid_9'><script>_avp.push({ tagid: 'avp_zid_9', alias: '/', type: 'banner', zid: 9, pid: 53 });</script></div><!-- END ADREACTOR CODE --></div>";
 					
-					var uploaddate = moment(msg.response[i].date*1000).format('LL');
+//					var uploaddate = moment(msg.response[i].date * 100).format('LL');
+					var uploaddate = "Not yet implemented" ;
+					console.log(msg.response[i]);
 					
-					var tablecontent = '<table class="table table-condensed table-responsive"><tbody><tr><td>Track</td><td>' + msg.response[i].title + '</td></tr><tr><td>Artist</td><td>' + msg.response[i].artist + '</td></tr><tr><td>Duration</td><td>' + msg.response[i].duration.toTime() + '</td></tr><tr><td>Genre</td><td>' + msg.response[i].genre_id + '</td></tr><tr><td>Upload Date</td><td>' + uploaddate +'</td></tr></tbody></table>';
+					var tablecontent = "<table class='table table-condensed table-responsive'><tbody><tr><td>Track</td><td>" + msg.response[i].title + '</td></tr><tr><td>Artist</td><td>' + msg.response[i].artist + '</td></tr><tr><td>Duration</td><td>' + msg.response[i].duration.toTime() + '</td></tr><tr><td>Genre</td><td>' + msg.response[i].genre + '</td></tr><tr><td>Upload Date</td><td>' + uploaddate + "</td></tr></tbody></table>";
 					
-					var link = '<a class="song" tabindex="0" data-toggle="popover" role="button" data-trigger="focus" data-placement="bottom" title="Song Info" data-html="true" data-content=" ' + tablecontent + popovercontent + ' ">' + msg.response[i].artist + ' - ' + msg.response[i].title + '</a>';
+					var link = '<a class="song" tabindex="0" data-toggle="popover" role="button" data-trigger="click" data-placement="bottom" title="Song Info" data-html="true" data-content=" ' + tablecontent + popovercontent + ' ">' + msg.response[i].artist + ' - ' + msg.response[i].title + '</a>';
 					
 					$('#result > .list-group').append('<li class="list-group-item">'+ savebutton + songlength + playbutton + link+'</li>');
 					
